c8e416d2a37540bfca3e58dbb098f4e28563ead1
ystalaya <ystalaya2009@gmail.com>
2020-11-17 16:57:24 +0300

    new component

---
+++ my-app/src/app/mynewcomponent/mynewcomponent.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MynewcomponentComponent } from './mynewcomponent.component';
+
+describe('MynewcomponentComponent', () => {
+  let component: MynewcomponentComponent;
+  let fixture: ComponentFixture<MynewcomponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MynewcomponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MynewcomponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ my-app/src/app/mynewcomponent/mynewcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mynewcomponent',
+  templateUrl: './mynewcomponent.component.html',
+  styleUrls: ['./mynewcomponent.component.css']
+})
+export class MynewcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
